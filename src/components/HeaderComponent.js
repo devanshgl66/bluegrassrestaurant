8320b3d0fbc57cfc862c4084bb37a16f465e72a4
devanshgl66 <devanshgl55@gmail.com>
2020-07-04 20:31:00 +0530

    minor change

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -195,7 +195,7 @@ class Header extends Component {
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/">
+                        <NavbarBrand className="mr-auto" href="/bluegrassrestaurant/home">
                         <img src='assets/images/logo.png' height="30" width="41" alt='Restaurant Blue Grass' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
